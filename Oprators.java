a9594e2e887ee7801018a14f3038b4c1cbf383e5
sarthak07072121 <sarthak07072121@gmail.com>
2026-06-02 09:42:30 +0530

    oparator in java

---
+++ Oprators.java
@@ -0,0 +1,18 @@
+public class Oprators {
+    
+    public static void main(String[] args)
+    {
+        int a = 10;
+        int b = 5;    
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(b/a);
+        System.out.println(a%b);
+        System.out.println("addition "+(a+b));  
+     
+    }
+
+
+}
